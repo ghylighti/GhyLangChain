3b8f2fb0faffe3b77fc017a2c1ae92d0d5c11757
13708207052@163.com <13708207052@163.com>
2025-04-04 23:40:36 +0800

    1

--- CmdMain.py
+++ CmdMain.py
@@ -1,7 +1,7 @@
 import asyncio
 import cmd
 import time
-import main
+import Controller
 from DB import IChroma
 from TTS.xhTTs import tts_websocket
 from TTS import PyGame
@@ -46,15 +46,15 @@ class SimpleCLI(cmd.Cmd):
     #问答系统
     def do_spk(self, arg):
         print(f"arg:{arg}==>role:{self.role}==>book:{self.book}")
-        result=main.query(arg,self.role,self.book)
-        ans=main.get_prompt(arg,result=result,role=self.role,book=self.book)
+        result=Controller.query(arg,self.role,self.book)
+        ans=Controller.get_prompt(arg,result=result,role=self.role,book=self.book)
         asyncio.run(tts_websocket(ans))
         time.sleep(1)  # 等待 0.5 秒再播放，确保文件写入完成
         PyGame.play()
         print(f"user question is {arg}")
     #加载知识库
     def do_load(self,arg):
-        main.load(arg)
+        Controller.load(arg)
         print(f"load html is {arg}")
     def do_role(self,arg):
         self.role=arg
