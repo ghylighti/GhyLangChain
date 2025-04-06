fbb636219e63ab759b54101cf3e567ad0b047654
13708207052@163.com <13708207052@163.com>
2025-04-06 15:06:23 +0800

    配置

--- Const/Const.py
+++ Const/Const.py
@@ -1,7 +1,7 @@
 # 配置星火模型的API URL和API密钥
-api_url = "https://spark-api-open.xf-yun.com/v1/chat/completions"  # 替换成你的星火模型API URL
-api_token = "nsVFfBjLOCzfmmgjVWFa:mRZLpSpSeiQhJWlFEzTG"  # 替换成你的API密钥
+api_url = ""  # 替换成你的星火模型API URL
+api_token = ""  # 替换成你的API密钥
 
-app_id = "1690d84f"  # 替换为您的 APPID
-api_key = "dc7ea49d5abe67227218ce8a44c04063"  # 替换为您的 APIKey
-api_secret = "NDE1MjQzZWMzYTk2ODExMjRlODQ5N2Yw"  # 替换为您的 APISecret
+app_id = ""  # 替换为您的 APPID
+api_key = ""  # 替换为您的 APIKey
+api_secret = ""  # 替换为您的 APISecret
